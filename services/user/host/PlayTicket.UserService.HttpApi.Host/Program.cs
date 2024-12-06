db72d7a4e421d6e87295a6f343cefa4d23c02f7e
nadeem <mohammadnadm5@gmail.com>
2024-12-06 12:33:26 +0530

    Updated the latest

--- services/user/host/PlayTicket.UserService.HttpApi.Host/Program.cs
+++ services/user/host/PlayTicket.UserService.HttpApi.Host/Program.cs
@@ -3,9 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PlayTicket.ServiceDefaults;
+using PlayTicket.Hosting.Shared;
 using Serilog;
-using Serilog.Events;
 
 namespace PlayTicket.UserService;
 
@@ -13,19 +12,14 @@ public class Program
 {
     public async static Task<int> Main(string[] args)
     {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Information()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
-            .Enrich.FromLogContext()
-            .WriteTo.Async(c => c.File("Logs/logs.txt"))
-            .CreateLogger();
+        var assemblyName = typeof(Program).Assembly.GetName().Name;
+
+        SerilogConfigurationHelper.Configure(assemblyName);
 
         try
         {
             Log.Information("Starting web host.");
             var builder = WebApplication.CreateBuilder(args);
-            builder.AddServiceDefaults();
             builder.Host.AddAppSettingsSecretsJson()
                 .UseAutofac()
                 .UseSerilog();
