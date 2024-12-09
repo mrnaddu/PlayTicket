542c4aea78104c3506d2c4b9f4cf964b919f699a
nadeem <mohammadnadm5@gmail.com>
2024-12-09 11:41:18 +0530

    Updated the latest

--- etc/run.ps1
+++ etc/run.ps1
@@ -1,5 +1,12 @@
 Invoke-Expression "./etc/setup-infra.ps1"
 
-tye run --watch
+# Navigate to the PlayTicket.AppHost and run the migration
+Set-Location -Path "./PlayTicket.AppHost"
+try {
+    dotnet run
+} catch {
+    Write-Error "Failed to run the .NET Aspire"
+    exit 1
+}
 
-#tye run --debug *
+
