1c1ca0950782f06d097dcc053d3b89cc712cc34d
nadeem <mohammadnadm5@gmail.com>
2024-12-09 10:28:38 +0530

    Updated latest

--- etc/docker/mysql-init/init-databases.sql
+++ etc/docker/mysql-init/init-databases.sql
@@ -1,7 +1,10 @@
+-- Create databases if they do not exist
 CREATE DATABASE IF NOT EXISTS db_office;
 CREATE DATABASE IF NOT EXISTS db_compliance;
 
+-- Grant all privileges on the databases to the root user
 GRANT ALL PRIVILEGES ON db_office.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON db_compliance.* TO 'root'@'%';
 
+-- Flush privileges to ensure that the changes take effect
 FLUSH PRIVILEGES;
